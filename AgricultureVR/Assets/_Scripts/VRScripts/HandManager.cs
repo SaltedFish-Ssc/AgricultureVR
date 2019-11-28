ad2f2cbf10cd1648eee8073d33de1ae1790bea06
LeChuangSSC <1822484495>
2019-11-28 18:25:09 +0800

    导入场景

---
+++ AgricultureVR/Assets/_Scripts/VRScripts/HandManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class HandManager : MonoBehaviour
+{
+    [SerializeField]
+    private VRTK_ControllerEvents _events;
+
+    /// <summary>
+    /// 手抓握动画
+    /// </summary>
+    public Animator _GrabAnimation; 
+
+    void Start()
+    {
+        _events.TriggerAxisChanged += _events_TriggerAxisChanged;
+    }
+
+    /// <summary>
+    /// 抓取键按下的角度
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void _events_TriggerAxisChanged(object sender, ControllerInteractionEventArgs e)
+    {
+        _GrabAnimation.Play("Grab",0,e.buttonPressure);
+        _GrabAnimation.speed = 0;
+    }
+
+    void Update()
+    {
+        
+    }
+}
